7da2c95b3e3acbe480c1bddd507baa231255baf2
Dmytro Shovheniuk <dimashovgenyuk06@gmail.com>
2025-07-19 08:15:26 -0500

    error correction

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,13 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  if (days <= 2) {
-    return days * 40;
-  } else if (days <= 6) {
-    return days * 40 - 20;
-  } else {
-    return days * 40 - 50;
+  const costPerDay = 40;
+  const shortTerm = 2;
+  const mediumTerm = 6;
+  const smallDiscount = 20;
+  const mediumDiscount = 50;
+
+  if (days <= shortTerm) {
+    return days * costPerDay;
+  } else if (days <= mediumTerm) {
+    return days * costPerDay - smallDiscount;
   }
+
+  return days * costPerDay - mediumDiscount;
 }
 
 module.exports = calculateRentalCost;
